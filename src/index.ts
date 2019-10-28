7cdc5ae95e961a7ce2eeba0a65e507e749cc67d6
Thibault Derousseaux <tibdex@users.noreply.github.com>
2019-10-28 20:51:25 +0100

    Get GitHub token as a required input (#14)
    It's more explicit than using a environment variable.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-import { debug, setFailed } from "@actions/core";
+import { debug, getInput, setFailed } from "@actions/core";
 import { context, GitHub } from "@actions/github";
 
 import { autosquash } from "./autosquash";
 
 const run = async () => {
   try {
-    const token = process.env.GITHUB_TOKEN;
-    if (token === undefined) {
-      throw new Error("Missing GITHUB_TOKEN environment variable");
-    }
-
+    const token = getInput("github_token", { required: true });
     debug(JSON.stringify(context, null, 2));
     const github = new GitHub(token);
     await autosquash({ context, github });
